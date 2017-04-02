12c846b75019f37c23dd103f077fbc4764594776
yuandrew2014 <league@WorthyMini-4.attlocal.net>
2017-04-02 11:30:22 -0700

    New repository

---
+++ src/LevelTwoGame.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class LevelTwoGame {
+
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.setSize(800, 800);
+		frame.add(panel);
+		frame.setVisible(true);
+
+	}
+
+}
